3b63a88bbe64d05e0b24158c90d6f811547c6a10
Ryan Khong <ryankhong@Khongs-MacBook-Pro.local>
2023-02-21 16:25:09 +0800

    init address file

--- Database/src/Storage/Address.java
+++ Database/src/Storage/Address.java
@@ -1,4 +1,32 @@
 package Storage;
 
 public class Address {
+    int blockId;
+    int offset;
+
+    public Address(int blockId, int offset){
+        this.blockId=blockId;
+        this.offset=offset;
+    }
+
+    public int getBlockId() {
+        return blockId;
+    }
+
+    public void setBlockId(int blockId) {
+        this.blockId = blockId;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("@%d-%d", blockId, offset);
+    }
 }
