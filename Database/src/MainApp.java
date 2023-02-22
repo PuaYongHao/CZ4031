4f53124cf8099ae3b2d5f3f5e892a99c7f37fe4b
c-zhihao <zhihaochin12@gmail.com>
2023-02-22 19:43:08 +0800

    mainApp updates to make it runnable

--- Database/src/MainApp.java
+++ Database/src/MainApp.java
@@ -1,17 +1,20 @@
-package app;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.util.*;
 
-import app.storage.Address;
-import app.storage.Disk;
-import app.storage.Record;
+import Storage.Address;
+import Storage.Disk;
+import Storage.Record;
 import Index.BPlusTree;
-import app.util.Log;
-import app.util.Utility;
+//import app.util.Log;
+//import app.util.Utility;
 
-public class MainApp implements Constants {
-	private static final String TAG = "App";
+public class MainApp {
+	//private static final String TAG = "App";
 	Scanner scanner = new Scanner(System.in);
 	private Disk disk;
 	private BPlusTree index;
@@ -19,21 +22,19 @@ public class MainApp implements Constants {
 
 	public void run(int blockSize) throws Exception {
 		// read records from data file
-		List<Record> records = Utility.readRecord(DATA_FILE_PATH);
+		List<Record> records = readRecord("data.tsv");
 
-		disk = new Disk(Constants.DISK_SIZE, blockSize);
+		disk = new Disk(500*1024*1024, blockSize);
 		index = new BPlusTree(blockSize);
-
-		Log.i(TAG,"Running program with block size of "+blockSize);
-		Log.i(TAG,"Prepare to insert records into storage and create index");
+		
+		System.out.println("Block size: " + blockSize);
 		Address recordAddr;
 		for (Record r: records) {
 			// inserting records into disk and create index!
 			recordAddr = disk.appendRecord(r);
 			index.insert( r.getNumVotes(), recordAddr);
 		}
-		Log.i(TAG,"Record inserted into storage and index created");
-		disk.log();
+		//disk.log();
 //		index.logStructure(1); // printing root and first level?
 
 		index.treeStats();
@@ -56,8 +57,8 @@ public class MainApp implements Constants {
 		LocalTime timeEnd = LocalTime.now(); //record end time
 		System.out.println("Start: "+timeStart+" End: "+timeEnd+" time lapse: " + timeEnd.compareTo(timeStart));
 		//retrieve all the actual records inside the block
-		ArrayList<Record> records = disk.getRecords(e3RecordAddresses);
-		System.out.println("Number of records found: "+ records.size());
+		//ArrayList<Record> records = disk.getRecords(e3RecordAddresses);
+		//System.out.println("Number of records found: "+ records.size());
 		
 		//bruteforce way
 		timeStart = LocalTime.now(); //record start time
@@ -67,24 +68,24 @@ public class MainApp implements Constants {
 
 		
 		double avgRating = 0;
-		for (Record record: records) {
-			avgRating += record.getAvgRating();
-		}
-		avgRating /= records.size();
-		Log.i("Average rating="+avgRating);
+//		for (Record record: records) {
+//			avgRating += record.getAvgRating();
+//		}
+//		avgRating /= records.size();
+		System.out.println("Average rating is: "+avgRating);
 	}
 
 	public void experiment4(){
-		Log.i(TAG,"Experience 4 started, getting records with numVotes between 30k-40k ");
+		//Log.i(TAG,"Experience 4 started, getting records with numVotes between 30k-40k ");
 		ArrayList<Address> e4RecordAddresses = index.getRecordsWithKeyInRange(30000,40000);
-		ArrayList<Record> records = disk.getRecords(e4RecordAddresses);
-		// records collected, do calculate average rating
-		double avgRating = 0;
-		for (Record record: records) {
-			avgRating += record.getAvgRating();
-		}
-		avgRating /= records.size();
-		Log.i("Average rating="+avgRating);
+//		ArrayList<Record> records = disk.getRecords(e4RecordAddresses);
+//		// records collected, do calculate average rating
+//		double avgRating = 0;
+//		for (Record record: records) {
+//			avgRating += record.getAvgRating();
+//		}
+//		avgRating /= records.size();
+//		System.out.println("Average rating is: "+avgRating);
 	}
 
 	public void experiment5(){
@@ -99,14 +100,52 @@ public class MainApp implements Constants {
 		System.out.print(message);
 		scanner.nextLine();
 	}
+	
+	private List<Record> readRecord(String path) throws Exception {
+		File dataFile = new File(path);
+		if (!dataFile.exists()) {
+			System.out.println("File does not exist... re-reading the file now...");
+
+			dataFile = new File("Database/src", path);
+			if (!dataFile.exists()){
+				throw new FileNotFoundException("File not exist");
+			}
+		}
+
+		BufferedReader br = null;
+		List<Record> records = new ArrayList<>();
+		String line;
+		String[] parts = null;
+		try {
+			br = new BufferedReader( new FileReader(dataFile));
+			// reading header first (to be skipped)
+			br.readLine();
+			while((line = br.readLine()) != null) {
+				parts = line.split("\\t");
+				Record record = new Record(parts[0], Float.parseFloat(parts[1]), Integer.parseInt( parts[2]));
+				records.add( record );
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				}catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		System.out.println("Total records: " + records.size());
+		return records;
+	}
 
 
 
 	public static void main(String[] args) {
 		try {
-			Log.setLevel(Log.LEVEL_DEBUG);
 			MainApp app = new MainApp();
-			app.run(BLOCK_SIZE_200);
+			app.run(200);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
