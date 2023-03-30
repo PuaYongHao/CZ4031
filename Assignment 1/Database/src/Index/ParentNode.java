c24cbe0cb8aef47d8418b79e5cb5ec5f247005d7
PuaYongHao <puayonghao@outlook.com>
2023-03-30 23:40:36 +0800

    Arrange the file

---
+++ Assignment 1/Database/src/Index/ParentNode.java
@@ -0,0 +1,135 @@
+package Index;
+
+import java.util.ArrayList;
+
+public class ParentNode extends Node {
+
+	// Child node could be leaf node or parent node
+	private ArrayList<Node> childNodes;
+	
+	// Constructor
+	public ParentNode() {
+		super();
+		childNodes = new ArrayList<Node>();
+	}
+	
+    // Get all child nodes
+    public ArrayList<Node> getChildNodes() {
+        return childNodes;
+    }
+
+    // Get child node at given index in this parent node
+    public Node getChildNodeAtIndex(int index) {
+        return childNodes.get(index);
+    }
+    
+    // Add child node to the appropriate position
+    public int addChildNode(Node childNode) {
+    	// If parent node is empty
+    	if (childNodes.size() == 0) {
+    		childNodes.add(childNode);
+    		// Set parent node of the child node to be current node
+    		// This is possible as a reference to the object is passed
+    		childNode.setParentNode(this);
+    		return 0;
+    	}
+    	
+    	int	key = childNode.findSmallestKey();
+    	int currentSmallest = this.findSmallestKey();
+    	int i;
+    
+    	// Index.Node that is going to be added has smaller key value
+    	if (key < currentSmallest) {
+    		// The leftmost child node key value is added
+    		this.addKey(currentSmallest);
+    		// Add the node to be the leftmost child node
+    		this.childNodes.add(0, childNode);
+    		i = 0;
+    	}
+    	else {
+    		i = this.addKey(key);
+    		// Pointer is key+1
+    		this.childNodes.add(++i, childNode);
+    	}
+    	
+    	childNode.setParentNode(this);
+    	return i;
+    }
+    
+    // Add child node at first index and reset the keys
+    public void addChildNodeAtFirstIndex(Node childNode) {
+    	this.childNodes.add(0, childNode);
+    	childNode.setParentNode(this);
+    	// Reset the keys
+    	deleteKeys();
+    	tallyKeysWithChildNodes();
+    }
+    
+    // Delete a child node and reset the keys
+    public void deleteChildNode(Node childNode) {
+    	this.childNodes.remove(childNode);
+    	// Reset the keys
+    	deleteKeys();
+    	tallyKeysWithChildNodes();
+    }
+    
+    // Reset the keys according to the child nodes
+    private void tallyKeysWithChildNodes() {
+        for (int i = 0; i < this.childNodes.size(); i++) {
+        	// The leftmost child node key value is not added
+            if (i != 0)
+                this.addKey(this.childNodes.get(i).findSmallestKey());
+        }
+    }
+    
+    // NEED TO REVIEW WITH OTHERS
+    // Delete all child nodes
+    public void deleteChildNodes() {
+    	// Make sure keys and child nodes are always tally
+    	deleteKeys();
+    	childNodes = new ArrayList<Node>();
+    }
+    
+    // Get the child node before
+    public Node getBefore(Node node) {
+    	// If it is not the first child node
+        if (childNodes.indexOf(node) != 0)
+            return childNodes.get(childNodes.indexOf(node) - 1);
+        return null;
+    }
+
+    // Get the child node after
+    public Node getAfter(Node node) {
+    	// If it is not the last child node
+        if (childNodes.indexOf(node) != childNodes.size() - 1)
+            return childNodes.get(childNodes.indexOf(node) + 1);
+        return null;
+    }
+
+	// NEED TO REVIEW WITH OTHERS
+	@Override
+	// Traverse the child node to find the smallest key
+	public int findSmallestKey() {
+		int key;
+		// If child node is leaf node
+		if (this.getIsLeaf())
+			key = this.getKeyAtIndex(0);
+		else {
+			ParentNode childNode = (ParentNode) this;
+			// Traverse if the node is not leaf node
+			while (!childNode.getChildNodeAtIndex(0).getIsLeaf())
+				childNode = (ParentNode) childNode.getChildNodeAtIndex(0);
+			key = childNode.getChildNodeAtIndex(0).getKeyAtIndex(0);
+		}
+		//key = this.getKeyAtIndex(0);
+		return key;
+	}
+	
+	// NEED TO REVIEW WITH OTHERS
+	// Delete current leaf node
+	public void deleteNode() {
+		super.deleteNode();
+		this.childNodes = new ArrayList<Node>();
+	}
+    
+}
---
+++ Assignment 1/Database/src/MainApp.java
@@ -0,0 +1,188 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.*;
+
+import Storage.Address;
+import Storage.Disk;
+import Storage.Record;
+import Index.BPlusTree;
+//import app.util.Log;
+//import app.util.Utility;
+
+public class MainApp {
+	// private static final String TAG = "App";
+	Scanner scanner = new Scanner(System.in);
+	private Disk disk;
+	private BPlusTree index;
+
+	//experiment 5 variable
+	private Disk experiment5Disk;
+	private BPlusTree experiment5Index;
+
+
+	public void run(int blockSize) throws Exception {
+		// read records from data file
+		List<Record> records = readRecord("data.tsv");
+
+		disk = new Disk(500 * 1024 * 1024, blockSize);
+		index = new BPlusTree(blockSize);
+		experiment5Disk = new Disk(500*1024*1024, blockSize);
+		experiment5Index = new BPlusTree(blockSize);
+		
+		System.out.println("Block size: " + blockSize);
+		Address recordAddr;
+		Address recordAddrExperiment5;
+		for (Record r: records) {
+			// inserting records into disk and create index!
+			recordAddr = disk.appendRecord(r);
+			index.insert( r.getNumVotes(), recordAddr);
+			recordAddrExperiment5 = experiment5Disk.appendRecord(r);
+			experiment5Index.insert( r.getNumVotes(), recordAddrExperiment5);
+		}
+
+		//disk.log();
+//		index.logStructure(1); // printing root and first level?
+
+		// index.treeStats();
+
+		// TODO do experiences
+		pause("Press any key to start experiment 1");
+		experiment1(records, blockSize);
+		pause("Press any key to start experiment 2");
+		experiment2();
+		pause("Press any key to start experiment 3");
+		experiment3();
+		pause("Press any key to start experiment 4");
+		experiment4();
+		pause("Press any key to start experiment 5");
+		experiment5();
+	}
+
+	public void experiment1(List<Record> records, int blockSize) {
+		System.out.println("Starting Experiment 1......");
+		System.out.println("Total number of records: " + records.size());
+		System.out.println("The size of a record: " + Record.size());
+		System.out.println("The number of records stored in a block: " + blockSize / Record.size());
+		System.out.println("The number of blocks for storing the data: " + records.size() / Record.size());
+	}
+
+	public void experiment2() {
+		System.out.println("Starting Experiment 2......");
+		index.treeStats();
+	}
+
+	public void experiment3() {
+		System.out.println("Starting Experiment 3......");
+		System.out.println("Search for numVotes == 500 ......");
+
+		// normal B+ scanning
+		LocalTime timeStart = LocalTime.now(); // record start time
+		ArrayList<Address> e3RecordAddresses = index.getRecordsWithKey(500);
+		LocalTime timeEnd = LocalTime.now(); // record end time
+		System.out.println("Start: " + timeStart + " End: " + timeEnd + " time lapse: " + timeEnd.compareTo(timeStart));
+		// retrieve all the actual records inside the block
+		ArrayList<Record> records = disk.getRecords(e3RecordAddresses);
+		System.out.println("Number of records found: " + records.size());
+
+		// bruteforce way
+		timeStart = LocalTime.now(); // record start time
+		ArrayList<Address> e3RecordAddressesBruteForce = index.getRecordsByBruteForce(500);
+		timeEnd = LocalTime.now(); // record end time
+		System.out.println("Start: " + timeStart + " End: " + timeEnd + " time lapse: " + timeEnd.compareTo(timeStart));
+
+		double avgRating = 0;
+		for (Record record : records) {
+			avgRating += record.getAvgRating();
+		}
+		avgRating /= records.size();
+		System.out.println("Average rating is: " + avgRating);
+	}
+
+	public void experiment4() {
+		// Log.i(TAG,"Experience 4 started, getting records with numVotes between
+		// 30k-40k ");
+		ArrayList<Address> e4RecordAddresses = index.getRecordsWithKeyInRange(30000, 40000);
+		ArrayList<Record> records = disk.getRecords(e4RecordAddresses);
+		// records collected, do calculate average rating
+		double avgRating = 0;
+		for (Record record : records) {
+			avgRating += record.getAvgRating();
+		}
+		avgRating /= records.size();
+		System.out.println("Average rating is: " + avgRating);
+	}
+
+	public void experiment5() {
+		System.out.println("Starting Experiment 5.............");
+		LocalTime timeStart = LocalTime.now(); // record start time
+		index.deleteKey(1000);
+		LocalTime timeEnd = LocalTime.now(); // record end time
+		System.out.println("Start: " + timeStart + " End: " + timeEnd + " time lapse: " + timeEnd.compareTo(timeStart));
+
+		// bruteforce
+		timeStart = LocalTime.now(); // record start time
+		index.deleteKeyByBruteForce(1000);
+		timeEnd = LocalTime.now(); //record end time
+		System.out.println("Start: "+timeStart+" End: "+timeEnd+" time lapse: " + timeEnd.compareTo(timeStart));
+	}
+
+	private void pause(String message) {
+		if (message == null) {
+			message = "Press any key to continue";
+		}
+		System.out.print(message);
+		scanner.nextLine();
+	}
+
+	private List<Record> readRecord(String path) throws Exception {
+		File dataFile = new File(path);
+		if (!dataFile.exists()) {
+			System.out.println("File does not exist... re-reading the file now...");
+
+			dataFile = new File("Database/src", path);
+			if (!dataFile.exists()) {
+				throw new FileNotFoundException("File not exist");
+			}
+		}
+
+		BufferedReader br = null;
+		List<Record> records = new ArrayList<>();
+		String line;
+		String[] parts = null;
+		try {
+			br = new BufferedReader(new FileReader(dataFile));
+			// reading header first (to be skipped)
+			br.readLine();
+			while ((line = br.readLine()) != null) {
+				parts = line.split("\\t");
+				Record record = new Record(parts[0], Float.parseFloat(parts[1]), Integer.parseInt(parts[2]));
+				records.add(record);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		// System.out.println("Total records: " + records.size());
+		return records;
+	}
+
+	public static void main(String[] args) {
+		try {
+			MainApp app = new MainApp();
+			app.run(200);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
