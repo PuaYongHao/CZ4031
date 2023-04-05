22ff425d3b9090bd7ba2f2220f39128c54a3ced4
c-zhihao <zhihaochin12@gmail.com>
2023-04-05 17:25:57 +0800

    add graph and ui improvements

--- Assignment 2/Code/README.md
+++ Assignment 2/Code/README.md
@@ -4,3 +4,4 @@ Steps to run the project:
 1. Open terminal at code directory
 2. pip install psycopg2
 3. pip install pyqt6
+4. pip install graphviz
--- Assignment 2/Code/UI.py
+++ Assignment 2/Code/UI.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt6.QtCore import Qt,QSize
 from PyQt6.QtWidgets import QComboBox,QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton
-
+from PyQt6.QtGui import QPixmap
+import graphviz
 import psycopg2
 
 
@@ -16,17 +17,23 @@ class MyWidget(QWidget):
     
     def __init__(self):
         super().__init__()
+
+        self.nodeList = list()
+        self.nodeCount = 1
         
         #Query1
         self.queryText1 = SquareLineEdit()
+        self.queryText1.textChanged.connect(self.carryOverText)
         
         self.queryDropDown1 = QComboBox()
+        self.queryDropDown1.addItem("None")
         self.queryDropDown1.addItem("Select * from customer")
         self.queryDropDown1.addItem("Select * from region")
         self.queryDropDown1.addItem("Select * from orders")
         self.queryDropDown1.currentIndexChanged.connect(self.updateLineEdit)
         
         generateButton1 = QPushButton()
+        generateButton1.setText("generate")
         
         explainText1 = SquareLineEdit()
         explainText1.setDisabled(True)
@@ -43,7 +50,7 @@ class MyWidget(QWidget):
         query1VL.addWidget(graphVizImage1)
 
         #Query2
-        queryText2 = SquareLineEdit()
+        self.queryText2 = SquareLineEdit()
         
         queryDropDown2 = QComboBox()
         queryDropDown2.addItem("Select * from customer")
@@ -51,20 +58,22 @@ class MyWidget(QWidget):
         queryDropDown2.addItem("Select * from orders")
         
         generateButton2 = QPushButton()
+        generateButton2.setText("generate")
         
         explainText2 = SquareLineEdit()
         explainText2.setDisabled(True)
     
-        #graphVizImage = QPixmap('sid.jpg')
-        graphVizImage2 = SquareLineEdit()
-        graphVizImage2.setDisabled(True)
+        #self.graphVizImage2 = QPixmap('sid.jpg')
+        self.graphVizImage2 = QLabel()
+        #graphVizImage2 = SquareLineEdit()
+        #graphVizImage2.setDisabled(True)
         
         query2VL = QVBoxLayout()
-        query2VL.addWidget(queryText2)
+        query2VL.addWidget(self.queryText2)
         query2VL.addWidget(queryDropDown2)
         query2VL.addWidget(generateButton2)
         query2VL.addWidget(explainText2)
-        query2VL.addWidget(graphVizImage2)
+        query2VL.addWidget(self.graphVizImage2)
         
         # Create title
         titleLabel = QLabel("Finally started on this project")
@@ -81,7 +90,99 @@ class MyWidget(QWidget):
         self.testingMessageLabel.setText(message)
 
     def updateLineEdit(self, index):
-        self.queryText1.setText(self.queryDropDown1.currentText())
+        index = self.queryDropDown1.currentIndex()
+        if index == 0:
+            self.queryText1.setText("")
+        else:
+            self.queryText1.setText(str(index))
+    def carryOverText(self):
+        self.queryText2.setText(self.queryText1.text())
+    
+    def treeDisplay(self, plan):
+        """
+            Function to display the traversal tree
+        """
+        f = graphviz.Graph()
+
+        #query = self.textEdit.toPlainText()
+        #plan = self.dbObj.getQueryPlan(query)
+        
+        if type(plan) is not dict:
+            self.el1.setText(f'ERROR: Tree visualisation failed. Please check your query.\n{plan}')
+        else:
+            adjList = self.getAdjList(plan, {})[0]
+            nodeList = self.nodeList
+
+            self.nodeCount = 1
+            self.nodeList = list()
+
+            for node in nodeList:
+                name = node.split('#')[0]
+                f.node(node, name)
+
+            for annotate in adjList:
+                if len(adjList[annotate]) != 0:
+                    for annotateString in adjList[annotate]:
+                        f.edge(annotate, annotateString)
+
+            f.render("QueryPlan", format="png", view=False)
+
+            self.im = QPixmap("./QueryPlan.png")
+            self.graphVizImage2.setPixmap(self.im)
+            self.graphVizImage2.setFixedHeight(self.im.size().height())
+            self.graphVizImage2.setFixedWidth(self.im.size().width())
+
+    def getAdjList(self, queryPlan, result):
+        """
+        This method performs a post order DFS traversal on the query plan (a nested dictionary) and returns an adjacency list of operators as a dict type
+            { parent node : [child node 1, child node 2, ...] }
+
+        We use a count to keep track of the number of nodes visited and append it to the end of each node to make it unique 
+        (else, there would be multiple Seq Scan's will be taken as the same node, when actually they are distinct Seq Scan's in the QEP)
+            {
+                "Hash Join#1" : ["Seq Scan#2", "Aggregate#3"], 
+                "Aggregate#3" : ["Sort#4", "Seq Scan#5"]
+            }
+        """
+
+        # Leaf node
+        if 'Plans' not in queryPlan:
+            if self.nodeCount == 1:
+                self.nodeList.append(
+                    f"{queryPlan['Node Type']}#{self.nodeCount}")
+            curIterCount = self.nodeCount
+            self.nodeCount += 1
+            return [result, curIterCount]
+
+        # Node with child nodes
+        else:
+            # Name the parent node, increment the count
+            planNodeType = f"{queryPlan['Node Type']}#{self.nodeCount}"
+            if planNodeType not in self.nodeList:
+                self.nodeList.append(planNodeType)
+
+            # Keep track of count in this recursion iteration
+            curIterCount = self.nodeCount
+            self.nodeCount += 1
+
+            # DFS: keep traversing until leaf node is reached
+            for subplan in queryPlan['Plans']:
+                nextIterCount = self.getAdjList(subplan, result)[1]
+
+                # Name the child node, increment the count
+                subplanNodeType = f"{subplan['Node Type']}#{nextIterCount}"
+                if subplanNodeType not in self.nodeList:
+                    self.nodeList.append(subplanNodeType)
+
+                # Add the child node to its parent node in the adjacency list
+                if planNodeType in result:
+                    result[planNodeType].append(subplanNodeType)
+                else:
+                    result[planNodeType] = [subplanNodeType]
+
+            # return curIterCount (OR, in the 1st iteration, return the final result)
+            return [result, curIterCount]
+
     
 
 
@@ -96,21 +197,23 @@ if __name__ == '__main__':
     cursor = conn.cursor()
     cursor.execute(
                 f"EXPLAIN (FORMAT JSON) {'''SELECT * from Region, customer Where region.r_regionkey = 0'''}")
+                #f"EXPLAIN (FORMAT JSON) {'''SELECT * from Region'''}")
     
     #cursor.execute('''SELECT * from Region''')
     result = cursor.fetchall()
     #print(result)
-    for x in result[0][0][0]["Plan"]:
-        if(x != "Plans"):
-            print("Key: ", x, " = ", result[0][0][0]["Plan"][x])
-    for y in result[0][0][0]["Plan"]["Plans"]:
-        print("-------------")
-        for z in y:
-            print("Key: ",z, " = ", y[z])
-    #print(result[0][0][0]["Plan"])
+    # for x in result[0][0][0]["Plan"]:
+    #     if(x != "Plans"):
+    #         print("Key: ", x, " = ", result[0][0][0]["Plan"][x])
+    # for y in result[0][0][0]["Plan"]["Plans"]:
+    #     print("-------------")
+    #     for z in y:
+    #         print("Key: ",z, " = ", y[z])
+    print(result[0][0][0])
     
     app = QApplication(sys.argv)
     widget = MyWidget()
     widget.setTestingMessage("hello")
     widget.show()
+    widget.treeDisplay(result[0][0][0]["Plan"])
     sys.exit(app.exec())
