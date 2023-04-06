206e9341f44db32dff16a990a540a05ef3891b9a
RTZJ <roystontzj@gmail.com>
2023-04-06 17:37:04 +0800

    Added Query 1 with enhanced 1

--- Assignment 2/Queries/1Enhanced1.sql
+++ Assignment 2/Queries/1Enhanced1.sql
@@ -1,21 +1,21 @@
 select
-	l_returnflag,
-	l_linestatus,
-	sum(l_quantity) as sum_qty,
-	sum(l_extendedprice) as sum_base_price,
-	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-	avg(l_quantity) as avg_qty,
-	avg(l_extendedprice) as avg_price,
-	avg(l_discount) as avg_disc,
-	count(*) as count_order
+	c_name as customer_name,
+	n_name as nation_name,
+	c_phone,
+	sum(o_totalprice) as total_spend
 from
-	lineitem
+	nation,customer,orders
 where
-	l_shipdate <= date '1998-12-01'
+	c_custkey = o_custkey AND
+	c_nationkey = n_nationkey AND
+	n_regionkey = 2 AND
+	c_custkey >= 10000 AND
+	c_acctbal < 0 AND
+	o_orderstatus = 'O' AND
+	o_orderdate >= DATE '1998-01-27'	
 group by
-	l_returnflag,
-	l_linestatus
+	c_custkey
 order by
-	l_returnflag,
-	l_linestatus;
+	total_spend DESC,
+	n_name,
+	c_name
