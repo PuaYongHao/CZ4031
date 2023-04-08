399655547544a95f2578b4fedf913726874bde07
PuaYongHao <puayonghao@outlook.com>
2023-04-08 17:02:38 +0800

    Update queries 3 and 4

--- Assignment 2/Queries/3Enhanced2.sql
+++ Assignment 2/Queries/3Enhanced2.sql
@@ -1,21 +1,19 @@
-select
-	l_returnflag,
-	l_linestatus,
-	sum(l_quantity) as sum_qty,
-	sum(l_extendedprice) as sum_base_price,
-	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-	avg(l_quantity) as avg_qty,
-	avg(l_extendedprice) as avg_price,
-	avg(l_discount) as avg_disc,
-	count(*) as count_order
+select 
+	r_name,
+	n_name,
+	c_mktsegment,
+	AVG(c_acctbal)
 from
-	lineitem
+	customer,
+	nation,
+	region
 where
-	l_shipdate <= date '1998-12-01'
+	n_nationkey = c_nationkey
+	and r_regionkey = n_regionkey
 group by
-	l_returnflag,
-	l_linestatus
+	r_name,
+	n_name,
+	c_mktsegment
 order by
-	l_returnflag,
-	l_linestatus;
+	r_name,
+	n_name
--- Assignment 2/Queries/4Enhanced2.sql
+++ Assignment 2/Queries/4Enhanced2.sql
@@ -1,21 +1,20 @@
 select
-	l_returnflag,
-	l_linestatus,
-	sum(l_quantity) as sum_qty,
-	sum(l_extendedprice) as sum_base_price,
-	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-	avg(l_quantity) as avg_qty,
-	avg(l_extendedprice) as avg_price,
-	avg(l_discount) as avg_disc,
-	count(*) as count_order
+	s_suppkey,
+	s_name,
+	total
 from
-	lineitem
+	supplier,
+	(
+		select
+			l_suppkey,
+			sum(l_extendedprice * (1 - l_discount)) AS total
+		from
+			lineitem
+		group by
+			l_suppkey
+	) as revenue
 where
-	l_shipdate <= date '1998-12-01'
-group by
-	l_returnflag,
-	l_linestatus
+	s_suppkey = l_suppkey
 order by
-	l_returnflag,
-	l_linestatus;
+	total
+LIMIT 1
